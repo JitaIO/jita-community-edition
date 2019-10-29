4ac37f616f633246e02f7815543cf1898c2ce468
Polley Wong <polley@gmail.com>
2019-10-29 18:19:48 +0800

    Removed an extra comma for console log #5

--- src/index.js
+++ src/index.js
@@ -15,5 +15,5 @@ const app = express();
 app.use('/'    , require('./routes/DefaultRoute'));
 
 app.listen(80, () => {
-    console.log("\nListening on \x1b[35m%s\x1b[0m:", "http://localhost");
+    console.log("\nListening on \x1b[35m%s\x1b[0m", "http://localhost");
 });
